7669d38c554bc1f7a0ff7564bdef7764ccf785c2
ryanlavin <rlavin@usc.edu>
2021-04-19 14:44:24 -0700

    Update to login

--- react-backend/src/components/login.js
+++ react-backend/src/components/login.js
@@ -1,11 +1,51 @@
 import React from "react";
 
-function Login() {
-    return (
-        <div>
-            <h1>Login Page!</h1>
-        </div>
-    );
+import FormInput from "./input-component";
+import '../css/login.css';
+
+function onClick() {
+    return null;
+}
+
+class Login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleChange = event => {
+        this.setState({
+            username : event.target.value,
+            password : event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>Login-In Form</h2>
+                <React.Fragment>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        />
+                    <button
+                        onPress = {this.handleChange}
+                        />
+                </React.Fragment>
+            </div>
+        );
+    }
 }
 
 export default Login;
+
