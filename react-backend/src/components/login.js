5eed7dcfa16fe967ddcb91bc3f204f67799ec6e5
Arman Roshannai <roshanna@usc.edu>
2021-04-21 04:16:18 -0700

    Made login page look fire

--- react-backend/src/components/login.js
+++ react-backend/src/components/login.js
@@ -1,65 +1,98 @@
 import React from "react";
-
+import "../css/login.css";
 import FormInput from "./input-component";
 //import '../css/login.css';
 
-
 function onClick() {
-    return null;
+  return null;
 }
 
 class Login extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        };
-    }
-
-
-    handleSubmit = event => {
-        event.preventDefault();
-        this.setState({username: '', password: ''});
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
     };
+  }
 
-    handleChange = event => {
-        const {value, name} = event.target;
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ username: "", password: "" });
+  };
 
-        this.setState({[name]: value});
-    };
+  handleChange = (event) => {
+    const { value, name } = event.target;
 
+    this.setState({ [name]: value });
+  };
 
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2>Login-In Form</h2>
+  render() {
+    return (
+      <div className="sign-in">
+        <div className="login">
+            <h2 id="login-header">Login</h2>
+            <div id="login-form-div">
                 <React.Fragment>
-		<form onSubmit={this.handleSubmit}>
+                <form id="login-form" onSubmit={this.handleSubmit}>
+                    <h2 id="username-header">Username</h2>
                     <FormInput
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        handleChange={this.handleChange}
-                        label="username"
-                        required
+                    id="username"
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    handleChange={this.handleChange}
+                    // label="username"
+                    required
                     />
+                    <h2 id="password-header">Password</h2>
                     <FormInput
-                        type="text"
-                        name="password"
-                        value={this.state.password}
-                        handleChange={this.handleChange}
-                        label="password"
-                        required
-                        />
-                    <button
-                        type="submit"> Sign In </button>
-		</form>
+                    id="password"
+                    type="text"
+                    name="password"
+                    value={this.state.password}
+                    handleChange={this.handleChange}
+                    // label="password"
+                    required
+                    />
+                    <button id="login-button" type="submit">Login</button>
+                </form>
                 </React.Fragment>
             </div>
-        );
-    }
+        </div>
+        <div className="register">
+        <h2 id="login-header">Register</h2>
+            <div id="login-form-div">
+                <React.Fragment>
+                <form id="login-form" onSubmit={this.handleSubmit}>
+                    <h2 id="username-header">Username</h2>
+                    <FormInput
+                    id="username"
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    handleChange={this.handleChange}
+                    // label="username"
+                    required
+                    />
+                    <h2 id="password-header">Password</h2>
+                    <FormInput
+                    id="password"
+                    type="text"
+                    name="password"
+                    value={this.state.password}
+                    handleChange={this.handleChange}
+                    // label="password"
+                    required
+                    />
+                    <button id="login-button" type="submit">Connect to Spotify</button>
+                </form>
+                </React.Fragment>
+            </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
-
